2ec7d40578fcf48f494818494a8fa45e784f9ffe
Snehpatel007 <81500980+Snehpatel007@users.noreply.github.com>
2023-02-09 14:29:08 +0530

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Button.css";
+
+function Button() {
+  return (
+    <div class="animation-trigger">
+      <div class="more-wrap">
+        <div class="more-text">Read more</div>
+        <div class="more-dots">
+          <div class="dot dot-1"></div>
+          <div class="dot dot-2"></div>
+          <div class="dot dot-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Button;
